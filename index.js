a1a044a04168b114b70dfcee4733e2dd20016158
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-08-12 16:41:11 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr,name) {
+    const ans = arr.filter(item => {
+        if(item.toUpperCase() === name.toUpperCase()){
+            return item
+        }
+        
+    })
+    return ans
+}
+function fuzzyMatch(arr,name) {
+    return arr.filter(item => item.startsWith(name))
+}
+function matchName(arr,str) {
+    return arr.filter(item => item.name === str)
+}
